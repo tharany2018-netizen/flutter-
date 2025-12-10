35a67791cf7ff7c25a276ee73e32454648551848
tharany2018-netizen <tharany2018@gmail.com>
2025-12-10 22:18:30 +0530

    Add files via upload

---
+++ padding1.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text(" Widget Tutorials"),
+        ),
+        body: const Padding(
+          padding:EdgeInsetsGeometry.all(8.0) ,
+          child: Text("padding widget"),
+   
+        ),
+      ));
+  }}
